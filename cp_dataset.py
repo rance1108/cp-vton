aa65c90f7971748ede14db29ea532964a33eebb3
rance <r06944042@ntu.edu.tw>
2020-06-16 20:46:06 +0800

    aaa

--- cp_dataset.py
+++ cp_dataset.py
@@ -58,6 +58,7 @@ class CPDataset(data.Dataset):
                 if osp.isfile(c_path) and osp.isfile(cm_path):
                     c.append(Image.open(c_path))
                     cm.append(Image.open(cm_path))
+                    print(cm)
                     if_c.append(True)
                 else:
                     c.append(Image.new('RGB',(102,147)))
