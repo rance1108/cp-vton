583c5e8fb89c570333b96c4e89464b6995e9ac0d
rance <r06944042@ntu.edu.tw>
2020-07-23 20:32:21 +0800

    aa

--- test.py
+++ test.py
@@ -101,8 +101,10 @@ def test_gmm(opt, test_loader, model, board):
                        [warped_grid[i], (warped_cloth[i]+im)*0.5, im]])
             print(c_names[i][0][:-4] +'_wc.png',c_names[i][0][:-4] +'_wcm.png')
             print((warped_cloth[i]).shape,warped_mask[i].shape)
-            save_images(warped_cloth[i], c_names[i][0][:-4] +'_wc.png' , warp_cloth_dir) 
-            save_images(warped_mask[i]*2-1, c_names[i][0][:-4] +'_wcm.png', warp_cloth_dir) 
+            cname1 = c_names[i][0][:-4] +'_wc.png'
+            cname2 = c_names[i][0][:-4] +'_wcm.png'
+            save_images(warped_cloth[i], cname1, warp_cloth_dir) 
+            save_images(warped_mask[i]*2-1, cname2, warp_cloth_dir) 
         
 
         # save_images(warped_cloth, c_names, warp_cloth_dir) 
