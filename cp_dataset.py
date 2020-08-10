6100f1daacce58bea50ca7c92d610d3489a9c7df
rance <r06944042@ntu.edu.tw>
2020-08-10 22:37:26 +0800

    aa

--- cp_dataset.py
+++ cp_dataset.py
@@ -45,16 +45,15 @@ class CPDataset(data.Dataset):
     def name(self):
         return "CPDataset"
 
-    def get_concat_h(self, im1, im2):
-        dst = Image.new('RGB', (im1.width + im2.width, im1.height))
-        dst.paste(im1, (0, 0))
-        dst.paste(im2, (im1.width, 0))
-        return dst
-
-
     def __getitem__(self, index):
         im_name = self.im_names[index]
 
+        def get_concat_h(im1, im2):
+            dst = Image.new('RGB', (im1.width + im2.width, im1.height))
+            dst.paste(im1, (0, 0))
+            dst.paste(im2, (im1.width, 0))
+            return dst
+            
         # cloth image & cloth mask
         if self.stage == 'GMM':
             c = []
