1b2683756b8544ba25555d09edb8ab7de9538e1f
rance <r06944042@ntu.edu.tw>
2020-08-10 22:55:39 +0800

    aa

--- train.py
+++ train.py
@@ -69,9 +69,9 @@ def train_gmm(opt, train_loader, model, board):
         im_c =  inputs['parse_cloth'].cuda()
         im_g = inputs['grid_image'].cuda()
         head_mask = inputs['head_mask'].cuda()
-        if_c = inputs['if_c']
-        import pdb 
-        pdb.set_trace()
+        if_c = inputs['if_c'].cuda()
+        # import pdb 
+        # pdb.set_trace()
         # grid, theta = model(agnostic, c)
         # warped_cloth = F.grid_sample(c, grid, padding_mode='border')
         # warped_mask = F.grid_sample(cm, grid, padding_mode='zeros')
