1dd8bb71bb388ce1ee8e003999be7b37805c59aa
rance <r06944042@ntu.edu.tw>
2020-08-11 21:28:07 +0800

    aa

--- train.py
+++ train.py
@@ -167,7 +167,7 @@ def train_tom(opt, train_loader, model, board):
 
             input_agnostic = torch.cat([agnostic,c[:,i]],dim=1)
             outputs = model(input_agnostic)
-            
+
             p_rendered, m_composite = torch.split(outputs, 3,1)
             p_rendered = F.tanh(p_rendered)
             m_composite = F.sigmoid(m_composite)
@@ -250,7 +250,7 @@ def main():
         train_gmm(opt, train_loader, model, board)
         save_checkpoint(model, os.path.join(opt.checkpoint_dir, opt.name, 'gmm_final.pth'))
     elif opt.stage == 'TOM':
-        model = UnetGenerator(24, 4, 6, ngf=64, norm_layer=nn.InstanceNorm2d)
+        model = UnetGenerator(23, 4, 6, ngf=64, norm_layer=nn.InstanceNorm2d)
         if not opt.checkpoint =='' and os.path.exists(opt.checkpoint):
             load_checkpoint(model, opt.checkpoint)
         train_tom(opt, train_loader, model, board)
