8330f479a6bc2b8b7aeac2b8ff26a9456c46887d
rance <r06944042@ntu.edu.tw>
2020-08-18 02:43:11 +0800

    aaa

--- train.py
+++ train.py
@@ -211,7 +211,7 @@ def train_tom(opt, train_loader, model, board):
                [c[:,2], cm[:,2]*2-1, m_composite[:,2]*2-1],
                [c[:,3], cm[:,3]*2-1, m_composite[:,3]*2-1], 
                [p_rendered, p_tryon, im_nobg]])
-        print(m_composite.shape,cm[:,0].shape,m_composite[:,0].shape)
+        print(m_composite.shape,cm.shape,cm[:,0].shape,m_composite[:,0].shape)
         for i in range(4):
             loss_mask += criterionMask(m_composite[:,i], cm[:,i])
 
