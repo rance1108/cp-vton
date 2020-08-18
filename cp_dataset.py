d32cf14d972d0c14af92468045b1125625902e85
rance <r06944042@ntu.edu.tw>
2020-08-19 00:52:27 +0800

    aaa

--- cp_dataset.py
+++ cp_dataset.py
@@ -168,8 +168,8 @@ class CPDataset(data.Dataset):
             print(n,len(if_c))
             if i == False:
                 parse_cloth.append(torch.from_numpy((parse_array > 5).astype(np.float32)))
-            elif n< len(if_c):
-                print(n+2)
+            elif n< len(if_c)-2:
+                print(n,n+2)
                 parse_cloth.append(torch.from_numpy((parse_array == n+2).astype(np.float32)))
             elif n == len(if_c):
                 print("shoe")
