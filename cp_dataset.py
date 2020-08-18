6d7804f372b9adbe27fc095c533be4c04afe9775
rance <r06944042@ntu.edu.tw>
2020-08-19 02:48:02 +0800

    aa

--- cp_dataset.py
+++ cp_dataset.py
@@ -203,17 +203,17 @@ class CPDataset(data.Dataset):
 
 
         for n,i in enumerate(if_c):
-            print(n,len(if_c))
             if i == False:
                 parse_cloth.append(torch.from_numpy((parse_array > 5).astype(np.float32)))
-            elif n < len(if_c)-1:
-                parse_cloth.append(torch.from_numpy((parse_array == n+2).astype(np.float32)))
             else:
-                print("hi")
-                im_parse_shoe = Image.open(osp.join(self.data_path, im_name, "10.png"))
-                parse_array_shoe = np.array(im_parse_shoe)
-                parse_cloth.append(torch.from_numpy((parse_array_shoe == 18).astype(np.float32)))
-                parse_cloth.append(torch.from_numpy((parse_array_shoe == 19).astype(np.float32)))
+                if n < len(if_c)-2:
+                    parse_cloth.append(torch.from_numpy((parse_array == n+2).astype(np.float32)))
+                else:
+                    im_parse_shoe = Image.open(osp.join(self.data_path, im_name, "10.png"))
+                    parse_array_shoe = np.array(im_parse_shoe)
+                    parse_cloth.append(torch.from_numpy((parse_array_shoe == 18).astype(np.float32)))
+                    parse_cloth.append(torch.from_numpy((parse_array_shoe == 19).astype(np.float32)))
+                    break
 
         im_nobg = Image.open(osp.join(self.data_path, im_name, "9.png"))
         im_nobg = self.transform(im_nobg) # [-1,1]
