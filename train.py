ebc0056302f94ba222d981c8cd95ac04dd13b40c
rance <r06944042@ntu.edu.tw>
2020-09-03 23:34:49 +0800

    aa

--- train.py
+++ train.py
@@ -213,7 +213,7 @@ def train_gmm(opt, train_loader, G_A, G_B, D_A, D_B, board):
 
         # loss_mask = torch.max(1-torch.sum(M_unwarp_warp,dim=1),torch.tensor([0.]))
 
-        loss_mask = torch.sum(F.relu(torch.sum(M_unwarp_warp,dim=1)))
+        loss_mask = torch.sum(F.relu(1-torch.sum(M_unwarp_warp,dim=1)))
 
         # GAN loss D_A(G_A(A))
         loss_G_A = criterionGAN(D_A(C_unwarp_warp_after.contiguous().view(C_unwarp_warp_after.size(0),
