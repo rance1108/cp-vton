374b144f5f40a405bb8ccb3f1b880d2f1600144d
rance1108 <rance1108@gmail.com>
2021-01-26 17:19:27 +0800

    aa

--- train.py
+++ train.py
@@ -183,8 +183,8 @@ def train_gmm(opt, train_loader, G_A, G_B, D_A, D_B, board):
             board.add_scalar('loss_G_B', loss_G_B.item(), step+1)
 
             t = time.time() - iter_start_time
-            print('step: %8d, time: %.3f, loss_G_A: %4f, loss_G_B_GAN: %4f , loss_L1_B: %4f , loss_vgg_B: %4f ' \
-                % (step+1, t, loss_L1.item(), loss_G_B_GAN.item(), loss_L1_B.item(), loss_vgg_B.item()), flush=True)
+            print('step: %8d, time: %.3f, loss_G_A: %4f, loss_L1_B: %4f , loss_vgg_B: %4f ' \
+                % (step+1, t, loss_L1.item(), loss_L1_B.item(), loss_vgg_B.item()), flush=True)
             # print('step: %8d, time: %.3f, loss_G_A: %4f, loss_DA: %4f , loss_G_B_GAN: %4f , loss_L1_B: %4f , loss_vgg_B: %4f ' \
             #     % (step+1, t, loss_L1.item(), loss_DA.item(),loss_G_B_GAN.item(),loss_L1_B.item(),loss_vgg_B.item()), flush=True)
 
